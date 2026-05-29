3d258e107d73bf162980030490b6a1a7beb83f11
prokopenko <oksana.prokopenko@p2h.com>
2026-05-29 17:25:05 +0300

    Handle CORS preflight requests in SKQ locations function and update response headers for compliance

--- netlify/functions/skq-locations.js
+++ netlify/functions/skq-locations.js
@@ -1,8 +1,29 @@
 const SKQ_API_BASE_URL = process.env.SKQ_API_BASE_URL;
 
 exports.handler = async (event) => {
+
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ""
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return {
+      statusCode: 405,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: "Method Not Allowed"
+    };
   }
 
   const { zip } = JSON.parse(event.body || '{}');
